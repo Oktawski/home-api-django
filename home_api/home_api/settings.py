83c62b4d761c7f96691d2c3bce9528106ac1d632
Oktawski <oktawian.perlowski@gmail.com>
2022-11-08 00:00:17 +0100

    Added authentication app. Implemented registering new users and getting token for user

--- home_api/home_api/settings.py
+++ home_api/home_api/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'groceries.apps.GroceriesConfig',
+    'authentication.apps.AuthenticationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
